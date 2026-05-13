a20ed421cb9ee1da7d1a33d2345c3723ed1f3bac
aimlinux <1k8aioon@gmail.com>
2026-05-13 14:10:36 +0900

    commit

---
+++ test_4/target/debug/deps/test_4.d
@@ -0,0 +1,5 @@
+C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\test_4\target\debug\deps\test_4.d: src\main.rs
+
+C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\test_4\target\debug\deps\test_4.exe: src\main.rs
+
+src\main.rs:
---
+++ test_4/target/debug/test_4.d
@@ -0,0 +1,1 @@
+C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\test_4\target\debug\test_4.exe: C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\test_4\src\main.rs
