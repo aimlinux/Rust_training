0372fe60c25d03d7a5da4adfc85b481d926e3a0e
aimlinux <1k8aioon@gmail.com>
2026-05-13 10:55:07 +0900

    change rust fire...

---
+++ HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\HelloWorld\target\debug\HelloWorld.exe: C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\HelloWorld\src\main.rs
---
+++ HelloWorld/target/debug/deps/HelloWorld.d
@@ -0,0 +1,5 @@
+C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\HelloWorld\target\debug\deps\HelloWorld.d: src\main.rs
+
+C:\Users\kxiyt\OneDrive\高専関係\GitHub\Rust_training\HelloWorld\target\debug\deps\HelloWorld.exe: src\main.rs
+
+src\main.rs:
